c3ecefd3bcb104a5cafc4bd34fcb9170e2263a70
Kylin Chen <kylingoround@gmail.com>
2019-01-08 15:46:29 -0500

    Updates

---
+++ precache-manifest.581c468784c44ab8def96041b4cfe864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76272bb36a68b2a1e1c3",
+    "url": "/static/js/main.76272bb3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "76272bb36a68b2a1e1c3",
+    "url": "/static/css/main.240060a1.chunk.css"
+  },
+  {
+    "revision": "d7732a6cc15f9e030b15d1628e1d7778",
+    "url": "/index.html"
+  }
+];
